7862214e0e3053497560c93b9e1d7c67144e3cd7
shondulah riggins <shondulahriggins@gmail.com>
2021-06-25 16:27:16 -0400

    Last Changes

--- js/site.js
+++ js/site.js
@@ -1,28 +1,32 @@
-function printNumbers() {
-    let startNum = parseInt(document.getElementById("numOne").value);
-    let endNum = parseInt(document.getElementById("numTwo").value);
-    displayData(startNum, endNum);
-}
+function reverseString() {
+    //Get the User's Word
+    let regex = /[\W_]/g;
+    let userWord = document.getElementById("tacoCat").value;
+    let cleanedWord = userWord.replaceAll(" ", "");
+    cleanedWord = cleanedWord.toLowerCase().replace(regex, "");
+    let start = cleanedWord.length - 1;
+    let reverseWord = "";
+    //Frank
+    //[F,r,a,n,k
+    //[0,1,2,3.4]-1
+    //For Loop
+    for (let i = start; i >= 0; i--) {
+        reverseWord = reverseWord + cleanedWord[i];
+        //Better Code: reverseWord += cleanedWord[i}]
+    }
 
-//Display the numbers in a table
-function displayData(start, end) {
-    const myTemplate = document.getElementById("dataTemplate");
-    const resultsBody = document.getElementById("resultsBody");
-    resultsBody.innerHTML = "";
-    //Loop over values from start to end
-    for (let i = start; i <= end; i += 10) {
-        const dataRow = document.importNode(myTemplate.content, true);
-        dataRow.getElementById("col1").textContent = i;
-        dataRow.getElementById("col2").textContent = i + 1;
-        dataRow.getElementById("col3").textContent = i + 2;
-        dataRow.getElementById("col4").textContent = i + 3;
-        dataRow.getElementById("col5").textContent = i + 4;
-        dataRow.getElementById("col6").textContent = i + 5;
-        dataRow.getElementById("col7").textContent = i + 6;
-        dataRow.getElementById("col8").textContent = i + 7;
-        dataRow.getElementById("col9").textContent = i + 8;
-        dataRow.getElementById("col10").textContent = i + 9;
-        resultsBody.appendChild(dataRow);
+    let output = document.getElementById("tacoOutput");
+    output.innerText = reverseWord
+
+    //Compare userWord to reverseWord
+    let resultOutput = document.getElementById("palindromeOutput");
+
+    if (cleanedWord == reverseWord) {
+        resultOutput.innerText = "It's a Palindrome!";
+
+
+    } else {
+        resultOutput.innerText = "Sorry, Not a Palindrome!";
     }
 
 }
